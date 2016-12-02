bbcdcc574f8ebb008135df08841d9652a006e1c7
Derrick Bray <derrickbray@yahoo.com>
2016-12-02 14:57:13 -0600

    got started

--- database/migrations/1479600686800_create_users_table.js
+++ database/migrations/1479600686800_create_users_table.js
@@ -7,10 +7,19 @@ class UsersTableSchema extends Schema {
   up() {
     this.create('users', (table) => {
       table.increments();
-      // table.string('username', 80).notNullable().unique()
+      table.string('org_name', 80);
+      table.string('address', 80);
       table.string('email', 254).notNullable().unique();
       table.string('password', 60).notNullable();
-      // table.boolean('is_admin').default(false);
+
+
+      table.string('phone', 80);
+      table.string('website', 80);
+
+      table.boolean('is_admin').default(false);
+      table.boolean('is_approved').default(false);
+
+
       table.timestamps();
     });
   }
